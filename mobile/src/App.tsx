c937e2c8d4f775b053d8a375c82dd971bbbc56ac
Mateo <mateovaldivia3000@gmail.com>
2025-04-07 02:03:09 -0400

    routing

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Text, View } from 'react-native';
+import Home from './screens/Home';
+import Splash from './screens/Splash';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Text>
-      Aplicaciddon movil
-    </Text>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        {isLoading ? (
+          <Stack.Screen name="Splash" component={Splash} options={{headerShown : false}}/>
+        ) : (
+          <Stack.Screen name="Home" component={Home} options={{headerShown : false}}/>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
