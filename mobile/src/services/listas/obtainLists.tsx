49d51a8a244dc9b56988fb52fc3c163bd63d5ed4
Mateo <mateovaldivia3000@gmail.com>
2025-05-25 01:55:34 -0400

    ver mas tarde

--- mobile/src/services/listas/obtainLists.tsx
+++ mobile/src/services/listas/obtainLists.tsx
@@ -4,14 +4,12 @@ import {collection, getDocs, doc, setDoc} from 'firebase/firestore';
 export const obtainLists = async (userID: string): Promise<any[]> => {
   try {
     const listasRef = collection(db, 'users', userID, 'listas');
-    const querySnapshot = await getDocs(listasRef);
+    let querySnapshot = await getDocs(listasRef);
 
-    // Verificar si "ver_mas_tarde" existe
     const verMasTardeExiste = querySnapshot.docs.some(
       doc => doc.id === 'ver_mas_tarde',
     );
 
-    // Si no existe, crearla
     if (!verMasTardeExiste) {
       const nuevaRef = doc(db, 'users', userID, 'listas', 'ver_mas_tarde');
       await setDoc(nuevaRef, {
@@ -20,6 +18,7 @@ export const obtainLists = async (userID: string): Promise<any[]> => {
         description: 'Películas guardadas para ver luego',
         nombreLista: 'Ver más tarde',
       });
+      querySnapshot = await getDocs(listasRef);
     }
 
     let lists = querySnapshot.docs.map(doc => ({
@@ -27,7 +26,6 @@ export const obtainLists = async (userID: string): Promise<any[]> => {
       ...doc.data(),
     }));
 
-    // Filtrar listas autogeneradas
     lists = lists.filter(item => {
       return item.id !== 'me_gusta' && item.id !== 'vistos';
     });
